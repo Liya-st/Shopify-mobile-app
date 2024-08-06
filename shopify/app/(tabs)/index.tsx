e47b78e7f6a2b8255100f152a278d79227236470
yohannaaaa <yohannamerawi@gmail.com>
2024-08-06 10:52:27 +0300

    structure edit

--- shopify/app/(tabs)/index.tsx
+++ shopify/app/(tabs)/index.tsx
@@ -35,7 +35,6 @@ export default function HomeScreen() {
           <Link href='/signup'>Sign-up</Link>
         </Text>
       </View>
-      <Link href='/users/1'>users</Link>
     </View>
   );
 }
