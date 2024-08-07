a918849b9bee2515c24ef2f5d2c0153782174c79
Liya Tsegaye <124361861+Liya-st@users.noreply.github.com>
2024-08-07 17:54:06 +0300

    Product Details debug

--- shopify/app/ProductDetail.tsx
+++ shopify/app/ProductDetail.tsx
@@ -17,15 +17,15 @@ const ProductDetail: React.FC = () => {
 const product = Products.find((p) => p.id === id);
 
   return (
-    <View className="pt-10">
+    <View className="pt-10 h-full">
            <ScrollView>
-            <View className="m-6 rounded-md bg-gray-50 shadow-xl">
+            <View className="m-6 rounded-md bg-gray-50 shadow-xl ">
               <View className="flex-1 justify-center items-center">
                 <Image
                   source={imageMap[product.src]}
                   alt=""
                   resizeMode="cover"
-                  className="h-[200px] w-[150px]"
+                  className="max-h-[100px] max-w-full"
                 />
               </View>
               <View className="bg-white outline-none">
@@ -35,12 +35,12 @@ const product = Products.find((p) => p.id === id);
                 <Text className="p-2 text-sm text-gray-400 ">${product.price}</Text>
               </View>
             </View>
-            <View className = 'flex  gap-10'>
+            <View className = 'flex'>
               <Text className="pl-5 text-xl font-bold">Description</Text>
-              <Text className="p-5 text-md text-slate-800">
+              <Text className="p-5  text-lg text-slate-800">
                 {product.description}
               </Text>
-              <Button title="Add to Cart" onPress={() => console.log("pressed")} />
+              <Button title="Add to Cart" onPress={() => console.log("pressed")} className = ""/>
               <Button title="Buy Now" />
             </View>
           </ScrollView>
