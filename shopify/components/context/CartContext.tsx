f5c118ca7efdc7661bf1645ff7d226f156b2e683
yohannaaaa <yohannamerawi@gmail.com>
2024-08-07 12:45:58 +0300

    cart context added

---
+++ shopify/components/context/CartContext.tsx
@@ -0,0 +1,88 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+
+type CartProviderProps = {
+  children: ReactNode;
+};
+
+type CartContext = {
+  getItemQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removefromCart: (id: number) => void;
+};
+
+type cartItems = {
+  id: number;
+  quantity: number;
+};
+
+const CartContext = createContext({} as CartContext);
+
+export function useCart() {
+  return useContext(CartContext);
+}
+
+export function CartProvider({ children }: CartProviderProps) {
+  const [cartItems, setCartItems] = useState<cartItems[]>([]);
+
+  function getItemQuantity(id: number) {
+    return cartItems.find((item) => item.id === id)?.quantity || 0;
+    // so ezi gare if the item exists return item.quantity if not the item is not present so return zero
+  }
+
+  function increaseCartQuantity(id: number) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id) == null) {
+        return [...currItems, { id, quantity: 1 }];
+      }
+      // here return all of our curritems plus this new item and set its quantity to 1 here I am adding it
+      else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          }
+          // here since the item already exists add one more to it
+          else return item;
+          // or else return our item as is
+        });
+      }
+    });
+  }
+
+  function decreaseCartQuantity(id: number) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id)?.quantity === 1) {
+        return currItems.filter((item) => item.id !== id);
+      }
+      // here I am filtering my itmes to everything but the one with that id that has a quantity of 1
+      else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 };
+          }
+          // here since the item already exists subtract one more to it
+          else return item;
+          // or else return our item as is
+        });
+      }
+    });
+  }
+
+  function removefromCart(id: number) {
+    setCartItems((currItems) => {
+      return currItems.filter((item) => item.id !== id);
+    });
+  }
+  return (
+    <CartContext.Provider
+      value={{
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removefromCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
